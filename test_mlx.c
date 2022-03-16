7282f1db15c1e23498d13a5526324928aac25a7b
Samantha Le Huec <sle-huec@student.42.fr>
2022-03-16 19:37:18 +0100

    seg fault

--- test_mlx.c
+++ test_mlx.c
@@ -9,6 +9,33 @@ typedef struct s_setup
 	void *win;
 } t_setup;
 
+int key_move(int keycode, t_setup *setup)
+{	
+	char	*path_char = "./face.xpm";
+	//char	*relative_path = "./ground.xpm";
+	int		x;
+	int		y;
+	//int		w = 0;
+	//int		h = 0;
+	int		w1 = 192;
+	int 	h1 = 384;
+	//void 	*img = mlx_xpm_file_to_image(setup->mlx, relative_path, &x, &y);
+	void 	*img_char = mlx_xpm_file_to_image(setup->mlx, path_char, &x, &y);
+	void *nxt_img_char;
+
+	mlx_put_image_to_window(setup->mlx, setup->win, img_char, w1, h1);
+
+		while (keycode == 100 || keycode == 65364)
+		{
+			h1 += 96;
+			mlx_destroy_image(setup->mlx, img_char);
+			//mlx_xpm_file_to_image(setup->mlx, "ground.xpm", &w1, &h1);
+			nxt_img_char = mlx_xpm_file_to_image(setup->mlx, "./face_left_foot.xpm", &x, &y);
+			mlx_put_image_to_window(setup->mlx, setup->win, nxt_img_char, w1, h1);
+		}
+	return 0;
+}
+
 int key_close(int keycode, t_setup *setup)
 {
 	if (keycode == 65307)
@@ -26,11 +53,15 @@ int main()
 {
 	t_setup setup;
 	void 	*img;
-	char	*relative_path = "./parquet_96_2.xpm";
+	void 	*img_char;
+	char	*path_char = "./face.xpm";
+	char	*relative_path = "./ground.xpm";
 	int		x;
 	int		y;
 	int		w = 0;
 	int		h = 0;
+	int		w1 = 192;
+	int 	h1 = 384;
 
 	setup.mlx = mlx_init();
 	if (!setup.mlx)
@@ -47,8 +78,11 @@ int main()
 		h = 0;
 		w+=96;
 	}
+	img_char = mlx_xpm_file_to_image(setup.mlx, path_char, &x, &y);
+	mlx_put_image_to_window(setup.mlx, setup.win, img_char, w1, h1);
 	mlx_hook(setup.win, 2, (1L<<0), &key_close, &setup);	
 	mlx_hook(setup.win, 17, 0, &mouse_close, (&setup));
+	mlx_hook(setup.win, 2, (1L<<0), &key_move, &setup);	
 	mlx_loop(setup.mlx);
 	mlx_destroy_image(setup.mlx, img);
 	mlx_destroy_window(setup.mlx, setup.win);
